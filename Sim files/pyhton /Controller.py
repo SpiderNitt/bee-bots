5db6301f32915adc8d0169bdb267f27b24cd639e
karthiks1701 <karthiks1701@gmail.com>
2019-09-01 01:57:20 +0530

    python follow done

--- Sim files/pyhton /Controller.py
+++ Sim files/pyhton /Controller.py
@@ -20,7 +20,7 @@ print ('Program started')
 vrep.simxFinish(-1) 
 clientID=vrep.simxStart('127.0.0.1',19999,True,True,5000,5) 
 if clientID!=-1:
-        pos_on_path=0
+        pos_on_path=1
         dis=0
         path_pos={}
         _,robotHandle=vrep.simxGetObjectHandle(clientID,'Start',vrep.simx_opmode_oneshot_wait)
@@ -28,33 +28,39 @@ if clientID!=-1:
         _,lm=vrep.simxGetObjectHandle(clientID,'LeftJoint',vrep.simx_opmode_oneshot_wait)
         _,rm=vrep.simxGetObjectHandle(clientID,'RightJoint',vrep.simx_opmode_oneshot_wait)
         _,ebot=vrep.simxGetObjectHandle(clientID,'eBot',vrep.simx_opmode_oneshot_wait)
-        print(vrep.simxGetObjectPosition(clientID,ebot,-1,vrep.simx_opmode_oneshot_wait))
+        print(vrep.simxGetObjectPosition(clientID,robotHandle,-1,vrep.simx_opmode_oneshot_wait))
         emptyBuff=bytearray()
         end=[2.5,2.5,0]
-        res,retInts,retFloats,retStrings,retBuffer=vrep.simxCallScriptFunction(clientID,'Dummy',vrep.sim_scripttype_childscript,'threadFunction',[],[],[],emptyBuff,vrep.simx_opmode_blocking)
+        res,retInts,retFloats,retStrings,retBuffer=vrep.simxCallScriptFunction(clientID,'Dummy',vrep.sim_scripttype_childscript,'threadFunction',[],[],[],emptyBuff,vrep.simx_opmode_oneshot_wait)
         if res==vrep.simx_return_ok:
             print ('Return value: ') 
         else:
             print ('Remote function call failed')
-        
+        print(len(retFloats))
         while(1):
-            path_pos={retFloats[pos_on_path],retFloats[pos_on_path+1],retFloats[pos_on_path+2]}
-            _=vrep.simxSetObjectPosition(clientID,robotHandle,-1,path_pos,vrep.simx_opmode_oneshot_wait)
-            _,_,[dis,phi],_,retBuffer=vrep.simxCallScriptFunction(clientID,'Dummy',vrep.sim_scripttype_childscript,'follow',[],path_pos,[],emptyBuff,vrep.simx_opmode_blocking)
-            #print(path1_pos)
-            #dis=math.sqrt((path1_pos[0])**2+(path1_pos[1])**2) 
-            print(dis)
-            #phi=math.atan2(path1_pos[1],path1_pos[0])
+            
+            _,_,dis,_,retBuffer=vrep.simxCallScriptFunction(clientID,'Dummy',vrep.sim_scripttype_childscript,'follow',[pos_on_path],retFloats,[],emptyBuff,vrep.simx_opmode_blocking)
+            
             v_des = -0.05
-            om_des = -2*phi
+            om_des = -0.8*dis[1]
             d=0.06
             v_r=(v_des+d*om_des)
             v_l=(v_des-d*om_des)
             r_w=0.0275 
-            omega_right=v_r/r_w
-            omega_left=v_l/r_w
-            _=vrep.simxSetJointTargetVelocity(clientID,lm,-omega_left,vrep.simx_opmode_oneshot_wait)
-            _=vrep.simxSetJointTargetVelocity(clientID,rm,-omega_right,vrep.simx_opmode_oneshot_wait) 
-            if(dis<0.1):
+            omega_right=(v_r/r_w)
+            omega_left=(v_l/r_w)
+            print(omega_right)
+            print(omega_left)
+            _=vrep.simxSetJointTargetVelocity(clientID,lm,(-1*omega_left),vrep.simx_opmode_oneshot_wait)
+            _=vrep.simxSetJointTargetVelocity(clientID,rm,(-1*omega_right),vrep.simx_opmode_oneshot_wait) 
+            
+            if(dis[0]<0.1):
                 pos_on_path+=3  
-            print(pos_on_path)
+                print(pos_on_path)
+            if(pos_on_path-1==len(retFloats)):
+                 
+                 _=vrep.simxSetJointTargetVelocity(clientID,lm,0,vrep.simx_opmode_oneshot_wait)
+                 _=vrep.simxSetJointTargetVelocity(clientID,rm,0,vrep.simx_opmode_oneshot_wait) 
+                 break
+            
+            

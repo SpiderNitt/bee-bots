0cc724bf68e1098b5d34a5b2d31fc0015e3fdd06
adityaravichander <aditya.ravichander@gmail.com>
2019-09-27 00:16:52 +0530

    hardware ultrasonic sensor test done

---
+++ Hardware /ultrasonic _test/ultrasonic_test.ino
@@ -0,0 +1,27 @@
+#define trigger 4
+#define echo 7
+ 
+float t=0,distance=0;
+ 
+void setup()
+{
+ pinMode(trigger,OUTPUT);
+ pinMode(echo,INPUT);
+ Serial.begin(9600);
+}
+ 
+void loop()
+{
+ digitalWrite(trigger,LOW);
+ delayMicroseconds(2);
+ digitalWrite(trigger,HIGH);
+ delayMicroseconds(8);
+ digitalWrite(trigger,LOW);
+ delayMicroseconds(2);
+ t=pulseIn(echo,HIGH); 
+ distance=((t*340)/20000);
+
+ Serial.print(" Distance(in cm) :");
+ Serial.println(distance);
+}
+

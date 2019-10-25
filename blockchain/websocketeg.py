e1a0b7a516a75414bf1d93cfe518369b9afaef07
Amogh J <amoghjahagirdargsf@gmail.com>
2019-10-25 18:40:00 +0530

    bug fixes

--- blockchain/websocketeg.py
+++ blockchain/websocketeg.py
@@ -10,19 +10,19 @@ all_chains = []
 max_pair = (-1,)
 # tuple of max length and index
 async def main():
-    count =0
-    while count < 4:
-        curr_chain, id = await websocket.recv()
-        all_chains.append(curr_chain)
-
-        if(max_pair[0]<curr_chain.get_chain_length):
-            max_pair = (curr_chain.get_chain_length,count)
-
-        count+=1
-    websocket.send(all_chains[max_pair[1]])
     uri = "ws://localhost:8765"
     async with websockets.connect(uri) as websocket:
-        print(f"> running")
+        # print(f"> running")
+        count =0
+        while count < 4:
+            curr_chain, id = await websocket.recv()
+            all_chains.append(curr_chain)
+
+            if(max_pair[0]<curr_chain.get_chain_length):
+                max_pair = (curr_chain.get_chain_length,count)
+
+            count+=1
+        websocket.send(all_chains[max_pair[1]])
 
 
 start_server = websockets.serve(main, "localhost", 8765)
